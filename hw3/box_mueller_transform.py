c83d9e218c218e23a200f6b8ae5dff1341360d95
Andrew Kirkham <andrew.nicholas.kirkham@gmail.com>
2016-02-28 20:07:37 -0600

    box_mueller work

--- hw3/box_mueller_transform.py
+++ hw3/box_mueller_transform.py
@@ -1,8 +1,6 @@
 #!/bin/python3
 import numpy
-import plotly
 from random import random
-from plotly.graph_objs import Histogram, Layout
 
 def generate_uniform_variables():
     u1 = [random() for i in range(10000)]
@@ -15,39 +13,14 @@ def box_muller_transformation(u1, u2):
     z2 = numpy.sqrt(-2*numpy.log(u1))*numpy.sin(2*numpy.pi*u2)
     return z1, z2
 
-def calculate_stats(z_cos, z_sin):
-    z = z_sin + z_cos
-    #calculate mean
-    mean = numpy.mean(z)
-    print('Mean: ' + str(mean))
-    #calculate variance
-    variance = numpy.var(z)
-    print('Variance: ' + str(variance))
-   
-
-#plot the distributions of each random variable
-def plot_results(u1, u2, z_cos, z_sin):
-    trace1 = Histogram(x=u1, name='Uniform Variable 1')
-    trace2 = Histogram(x=u2, name='Uniform Variable 2')
-    trace3 = Histogram(x=z_cos, name='Normal Variable z-cos')
-    trace4 = Histogram(x=z_sin, name='Normal Variable z-sin')
-    figure = plotly.tools.make_subplots(rows=2, cols=2)
-    figure.append_trace(trace1, 1, 1)
-    figure.append_trace(trace2, 1, 2)
-    figure.append_trace(trace3, 2, 1)
-    figure.append_trace(trace4, 2, 2)
-    figure['layout'].update(title='Box Muller Transform')
-
-    plotly.offline.plot(figure, filename="q1.html")
-
 def main():
-    z_cos = []
-    z_sin = []
+    z = []
     u1, u2 = generate_uniform_variables()
     for iRandom in range(10000):
         z1, z2 = box_muller_transformation(u1[iRandom-1], u2[iRandom-1])
-        z_cos.append(z1)
-        z_sin.append(z2)
+        z.append(z1)
+        z.append(z2)
+    return z
     
 if __name__=="__main__":
     main()
--- hw3/chi_squared_test.py
+++ hw3/chi_squared_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import scipy
+import scipy.stats
 from scipy.stats import chi2
 from scipy.integrate import quad
 import numpy
@@ -30,26 +31,52 @@ def calculate_chi2(random, bins, integrand):
     return bins/n * sum(chi_squared)
 
 def lcg():
-    print("\nLCG from HW1 for uniform random numbers")
     random = linear_congruential_generator.generate_scaled_data(100, 2000)
     bins = 128
     integrand = lambda x : 1
-    actual = calculate_chi2(random, bins, integrand)
-    expected = chi2.ppf(.99, bins-1)
-    evaluate_results(actual, expected)
+
+    actual_chi2 = calculate_chi2(random, bins, integrand)
+    expected_chi2 = chi2.ppf(.99, bins-1)
+    
+    uniform_stats(random)
+    evaluate_results(expected_chi2, actual_chi2)
+
+def uniform_stats(rands):
+    expected_mean = (1-0)/2
+    actual_mean = numpy.mean(rands)
+    expected_var = (((1-0+1) ** 2) - 1)/12
+    actual_var = numpy.var(rands)
+    evaluate_stats(expected_mean, actual_mean, expected_var, actual_var)
 
 def built_in_rand():
-    print("\nPython random library for uniform random numbers")
     rands = [random() for k in range(10000)]
     bins = 1000
     integrand = lambda x : 1
-    actual = calculate_chi2(rands, bins, integrand)
-    expected = chi2.ppf(.99, bins-1)
-    evaluate_results(actual, expected)
+    
+    actual_chi2 = calculate_chi2(rands, bins, integrand)
+    expected_chi2 = chi2.ppf(.99, bins-1)
+    
+    uniform_stats(rands)
+    evaluate_results(expected_chi2, actual_chi2)
     
 def box_mueller():
     #TODO
-    pass
+    rands=box_mueller_transform.main()
+    bins = 1000
+    integrand = lambda x: scipy.stats.norm.pdf(x)
+
+    actual_chi2 = calculate_chi2(rands, bins, integrand)
+    expected_chi2 = chi2.ppf(.99, bins-1)
+    
+    normal_stats(rands)
+    evaluate_results(expected_chi2, actual_chi2)
+
+def normal_stats(rands):
+    expected_mean = scipy.stats.norm.mean()
+    actual_mean = numpy.mean(rands)
+    expected_var = scipy.stats.norm.var()
+    actual_var = numpy.var(rands)
+    evaluate_stats(expected_mean, actual_mean, expected_var, actual_var)
 
 def cauchy():
     #TODO
@@ -59,8 +86,12 @@ def gamma():
     #TODO
     pass
 
-def evaluate_results(actual, expected):
-    print("Actual: ", actual, " Expected: ", expected)
+def evaluate_stats(expected_mean, actual_mean, expected_var, actual_var):
+    print("Expected mean: ", expected_mean, " Actual mean: ", actual_mean)
+    print("Expected var: ", expected_var, " Actual var: ", actual_var)
+
+def evaluate_results(expected, actual):
+    print("Expected chi2: ", expected, "Actual chi2: ", actual)
     if (actual > expected):
         print("Reject the null hypothesis! RNG poorly fits target")
     else:
@@ -68,8 +99,11 @@ def evaluate_results(actual, expected):
     
 
 def main():
+    print("\nLCG from HW1 for uniform random numbers")
     lcg()
+    print("\nPython random library for uniform random numbers")
     built_in_rand()
+    print("\nBox Mueller transform for normal random numbers")
     box_mueller()
     cauchy()
     gamma()
