2de5555029a84d62361455113643668f7be22fe2
Andrew Kirkham <andrew.nicholas.kirkham@gmail.com>
2016-03-31 11:50:04 -0500

    finishing 813.py

--- hw5/813.py
+++ hw5/813.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 import numpy
 import random
+from collections import Counter
 
 def main():
     data = [56, 101, 78, 67, 93, 87, 64, 72, 80, 69]
     mean=numpy.mean(data)
     a=-5
     b=5
-    num_trials=10
-    trial=[0 for x in range(num_trials)]
-    var_data = numpy.var(data)
+    num_trials=1000
+    trial=[]
+    
     for iTrial in range(num_trials): 
         bootstrap = [data[random.randint(0, len(data)-1)] for i in range(len(data))]
-        bootstrap_var=numpy.var(bootstrap)
-        trial[iTrial]=numpy.mean(bootstrap)
-    print([(x-mean) for x in trial])
+        trial.append(numpy.mean(bootstrap))
+    
+    difference=[(x-mean) for x in trial]
+    prob=[a < x < b for x in difference]
+    print('total probability: ', prob.count(True)/len(prob))
+
 if __name__=='__main__':
     main()
