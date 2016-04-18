e0845a45add5d79d24bd0528957e3b6c981769f0
Andrew Kirkham <andrew.nicholas.kirkham@gmail.com>
2016-04-17 20:34:38 -0500

    removing print statments and adding q2

--- hw6/q2.py
+++ hw6/q2.py
@@ -1,5 +1,6 @@
 #!/bin/python3
 import numpy
+from random import random
 from create_lattice import create_lattice
 from pad import pad
 
@@ -9,14 +10,17 @@ def main():
     j = 1
     n_configs = 250
     n_usable_configs = 200
-    temps = numpy.linspace(10,.5,20)
-    energy = numpy.zeros((len(temps), 1))
-    initial_energy = numpy.ones((l, l))
-    last_lattice = initial_energy
-    
-    for iTemp, temp in enumerate(temps):
-        lattice = create_lattice(l, h, j, last_lattice, n_configs, n_usable_configs, temp)
+    temps = numpy.linspace(10, .5, 20) 
+    specific_heat = numpy.zeros((len(temps), 1))
+
+    previous_lattice = [[round(random()) * 2 -1 for x in range(l)] for y in range(l)]
+    previous_lattice = numpy.array(previous_lattice)
+
+    for index, temp in enumerate(temps):
+        print(temp)
+        lattice = create_lattice(l,h,j, previous_lattice, n_configs, n_usable_configs, temp)
         lattice_pad = numpy.zeros((l+2, l+2, n_usable_configs))
+
         for config in range(n_usable_configs):
             lattice_pad[:,:,config] = pad(lattice[:,:,config])
         
@@ -26,15 +30,17 @@ def main():
         south = lattice_pad[2:end_x+1,1:end_y,:]
         east = lattice_pad[1:end_x,2:end_y+1,:]
         west = lattice_pad[1:end_x,0:end_y-1,:]
-        
+
         f = -1 * h * numpy.sum(lattice)
         s = -1 * j/2 * numpy.sum(lattice * 
-            (north + south + east + west))
-        energy_temp = (f+s)/n_usable_configs
-        energy[iTemp] = energy_temp
-        print('energy for ', temp, ' = ', energy_temp)
-        last_lattice = lattice[:,:,-1]
-    print(energy)
+                (north + south + east + west))
+        energy = (f+s)/n_usable_configs
+        heat = (energy**2/n_usable_configs - energy**2)/(temp**2) 
+        print(heat)
+        specific_heat[index] = heat 
+        previous_lattice = lattice[:,:,-1]
+
+
 
 if __name__=='__main__':
     main()
--- hw6/test_grid.py
+++ hw6/test_grid.py
@@ -9,7 +9,9 @@ def test_grid(h, j, lattice, r, c, temp_tilde):
     d_u = -((2*h*sigma) + 
             (2*j*sigma * 
                 (lattice[r+1,c] + lattice[r-1,c] + 
-                lattice[r,c+1] + lattice[r,c-1])))
+                lattice[r,c+1] + lattice[r,c-1])
+                )
+            )
     q = numpy.exp(-d_u/temp_tilde)
     probability = min(1, q)
 
